05e43ef611332a99e2397e454dc7df58117d2dd5
Gilbert Blankson-Afful <101939909+ekowamoonu@users.noreply.github.com>
2024-11-02 10:07:09 +0000

    Initial commit

---
+++ DemoLibrary/AccessDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class AccessDemo
+    {
+        //Access everywhere
+        public void PublicDemo() {
+            Console.WriteLine("Hello from public demo");
+        }
+
+        //Access only within class
+        private void PrivateDemo() { }
+
+        //Access only with assembly
+        internal void InternalDemo() { }
+
+        //Access only within class and derived classes. Not objects
+        protected void ProtectedDemo() { }
+
+        //Private outside assembly, protected within assembly
+        private protected void PrivateProtectedDemo() { }
+
+        //Protected outside assembly, internal within assmebly
+        protected internal void ProtectedInternalDemo() { }
+    }
+}
---
+++ ModifierUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using DemoLibrary;
+
+namespace ModifierUI
+{
+    class Progam
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, Access Modifiers!");
+            AccessDemo demo = new AccessDemo();
+            //demo.PublicDemo();
+            //demo.Process
+ 
+        }
+
+    }
+
+}
